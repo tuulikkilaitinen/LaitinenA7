61a01c4116b8c82cf94e899972283182ccc8f998
Tuulikki Laitinen <tuulikki.laitinen@gmail.com>
2017-05-16 17:31:47 -0700

    Initial commit

---
+++ app/src/main/java/sdccd/edu/laitinena7/MainActivity.java
@@ -0,0 +1,18 @@
+package sdccd.edu.laitinena7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // display logo in action bar
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+    }
+}
